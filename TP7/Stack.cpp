2b45de729408daf7b51c738552fce7fd44c72e09
BadakBercula7 <mufidizzuddin1@gmail.com>
2024-10-27 17:05:18 +0700

    Add files via upload

---
+++ TP7/Stack.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+void createStack (stack &S){
+    S.Top = -1;
+}
+
+bool isEmpty (stack S){
+    if (S.Top == -1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isFull (stack S){
+    if (S.Top == MAX -1){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void push (stack &S, infotype X){
+    if (!isFull (S)){
+        S.Top++;
+        S.info[S.Top] = X;
+    } else {
+        cout << "Stack penuh!" << endl;
+    }
+}
+
+int pop (stack &S){
+    if (!isEmpty (S)){
+        infotype X;
+        X = S.info[S.Top];
+        S.Top--;
+        return X;
+    } else {
+        cout << "Stack kosong!" << endl;
+        return 0;
+    }
+}
+
+
+
+int peek (stack &S){
+    if (!isEmpty (S)){
+        return S.info[S.Top];
+    } else{
+        cout << "Stack Kososng" << endl;
+        return -1;
+    }
+}
+
+//Soal 1
+int sumStack (stack S){
+    int sum = 0;
+    stack tempStack;
+    createStack(tempStack);
+
+    while(!isEmpty (S)){
+        int X = pop(S);
+        sum += X;
+        push (tempStack,X);
+    }
+
+    while (!isEmpty (tempStack)){
+        push (S,pop (tempStack));
+    }
+    return sum;
+}
+
+//Soal 2
+void reverseStack (stack &S){
+    cout << "Daftar anggota stack kondisi awal: ";
+    for (int i = S.Top; i >= 0; i--){
+        cout << (S.info [i]);
+        if (i > 0){
+            cout << "-" ;
+        }
+    }
+    cout << endl;
+
+    stack tempStack1,tempStack2;
+    createStack(tempStack1);
+    createStack(tempStack2);
+    while (!isEmpty (S)){
+        push(tempStack1,pop (S));
+    }
+    while (!isEmpty (tempStack1)){
+        push(tempStack2,pop (tempStack1));
+    }
+    while (!isEmpty (tempStack2)){
+        push(S,pop (tempStack2));
+    }
+    cout << "Daftar anggota stack setelah dibalik: ";
+    for (int i = S.Top; i >= 0; i--){
+        cout << (S.info [i]);
+        if (i > 0){
+            cout << "-" ;
+        }
+    }
+    cout << endl << endl;
+}
+
+//soal 3
+void printStack(stack &S) {
+    stack tempStack;
+    createStack(tempStack);
+
+    while (!isEmpty (S)) {
+        push(tempStack, pop(S));
+    }
+
+    cout << "Daftar elemen terurut dari paling bawah: ";
+    while (!isEmpty(tempStack)) {
+        int X = pop(tempStack);
+        cout << X;
+        if (!isEmpty (tempStack)){
+            cout << "-";
+        }
+        push(S, X);
+    }
+    cout << endl;
+}
+
---
+++ TP7/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+int main()
+{
+    stack S1,S2,S3;
+    createStack(S1);
+    createStack(S2);
+    createStack(S3);
+    int input;
+
+    //Soal 1
+    cout << "SOAL 1" <<endl;
+    cout << "input: ";
+    for (int i = 0; i < 8; i++){
+        cin >> input;
+        push (S1,input);
+    }
+    int hasil = sumStack (S1);
+    cout << "Hasil Penjumlhan: " << hasil << endl << endl;
+
+    //Soal 2
+    cout << "SOAL 2" <<endl;
+    cout << "input: ";
+    for (int i = 0; i < 8; i++){
+        cin >> input;
+        push (S2,input);
+    }
+
+    reverseStack (S2);
+
+    //Soal 3
+    cout << "SOAL 3" <<endl;
+    cout << "input: ";
+    for (int i = 0; i < 8; i++){
+        cin >> input;
+        push (S3,input);
+    }
+    printStack(S3);
+    return 0;
+}
